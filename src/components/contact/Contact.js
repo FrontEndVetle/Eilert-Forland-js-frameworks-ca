048d703df155d1fb5e24955e14d61b2e70eb69e6
Eilert Vetle Forland <ev.forland@gmail.com>
2020-12-08 13:32:12 +0100

    added contact page with yup validation and react-hook-form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React from 'react';
+import ContactForm from './ContactForm';
+import Heading from './../layout/Heading'
 
 
+export function Contact(){
+    return(
+          <div>
+              <Heading title="Contact"></Heading>
+        <ContactForm />
+    </div>
+    )
+}
 
 export default Contact;
---
+++ src/components/contact/ContactForm.js
@@ -0,0 +1,76 @@
+
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+
+const schema = yup.object().shape({
+    firstName: yup
+    .string()
+    .required("Please write your first name")
+    .min(2, "First Name must have minimum 2 letters"),
+
+    lastName: yup
+    .string()
+        .required("Please write your last name")
+        .min(2, "Last name must have minimum 2 letters"),
+
+    textArea: yup
+    .string()
+        .required("Please enter your message")
+        .min(10, "Message must be atleast 10 characters"),
+
+        eMail: yup
+    .string()
+        .required("Please write your Email")
+      .email(),
+
+});
+
+function ContactForm() {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+    function onSubmit(data) {
+        console.log("data", data);
+        alert("Thanks for your message");
+    }
+
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group>
+                <Form.Label>First name</Form.Label>
+                <Form.Control name="firstName" placeholder="Enter your first name" ref={register} />
+                {errors.firstName && <p>{errors.firstName.message}</p>}
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Last name</Form.Label>
+                <Form.Control name="lastName" placeholder="Enter your last name" ref={register} />
+                {errors.lastName && <p>{errors.lastName.message}</p>}
+            </Form.Group>
+
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control name="eMail" placeholder="Enter your Email" ref={register} />
+                {errors.eMail && <p>{errors.eMail.message}</p>}
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Send us message</Form.Label>
+                <Form.Control name="textArea" as="textarea" placeholder="Enter your message" rows={3} ref={register}/>
+                {errors.textArea && <p>{errors.textArea.message}</p>}
+
+            </Form.Group>
+
+            <Button type="submit">Submit</Button>
+        </Form>
+    );
+}
+
+export default ContactForm;
